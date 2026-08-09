56535c6e3fc0211e9a15671c770d0eadb9990434
Suyal Sahukhal <suyalsahukhala787@gmail.com>
2026-08-09 18:55:22 +0545

    feat: Add EF Core SQLite database, migrations, and product repository

---
+++ Repositories/SqliteProductRepository.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWebApiSandbox;
+
+public class SqliteProductRepository : IProductRepository
+{
+    private readonly AppDbContext _context;
+
+    public SqliteProductRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<Product> GetAll()
+    {
+        return _context.Products.ToList();
+    }
+
+    public Product? GetById(int id)
+    {
+        return _context.Products.FirstOrDefault(p => p.Id == id);
+    }
+
+    public void Add(Product newProduct)
+    {
+        _context.Products.Add(newProduct);
+        _context.SaveChanges();
+    }
+
+    public bool Update(int id, Product updatedProduct)
+    {
+        var product = GetById(id);
+        if (product == null)
+        {
+            return false;
+        }
+        product.Name = updatedProduct.Name;
+        product.Price = updatedProduct.Price;
+        _context.SaveChanges();
+        return true;
+    }
+
+    public bool Delete(int id)
+    {
+        var product = GetById(id);
+        if (product == null)
+        {
+            return false;
+        }
+        _context.Products.Remove(product);
+        _context.SaveChanges();
+        return true;
+    }
+}
