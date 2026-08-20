e634b937165b79a5202c53f0b059a5c6957e7605
Suyal Sahukhal <suyalsahukhala787@gmail.com>
2026-08-20 23:04:46 +0545

    test: xunit tests for CreateProduct with CreateProductDto and ProductResponseDto

--- AspNetCoreWebApiSandbox.Tests/ProductsControllerTests.cs
+++ AspNetCoreWebApiSandbox.Tests/ProductsControllerTests.cs
@@ -1,13 +1,27 @@
-using Xunit;
-using AspNetCoreWebApiSandbox;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreWebApiSandbox.Test;
-using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 namespace AspNetCoreWebApiSandbox.Tests;
 
 public class ProductsControllerTests
 {
+    [Fact]
+    public async Task CreateProduct_ReturnCreatedAtAction_WhenValid()
+    {
+        var fakeRepo = new FakeProductRepository();
+        var controller = new ProductsController(fakeRepo);
+        var newProduct = new CreateProductDto
+        {
+            Name = "Nokia 350",
+            Price = 199m
+        };
+        var result = await controller.CreateProduct(newProduct);
+        var createdResult = Assert.IsType<CreatedAtActionResult>(result);
+        var responseData = Assert.IsType<ProductResponseDto>(createdResult.Value);
+        Assert.Equal("Nokia 350", responseData.Name);
+        Assert.Equal(199m, responseData.Price);
+    }
+
     [Fact]
     public async Task GetProductById_ReturnsOk_WhenProductExists()
     {
